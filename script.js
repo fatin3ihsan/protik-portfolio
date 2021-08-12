689feaef8af1720b1f3d9e16366dbf02ac452170
fatin3ihsan <ihsanfatin3@gmail.com>
2021-08-13 00:06:56 +0600

    Basic Structure Done

---
+++ script.js
@@ -0,0 +1,14 @@
+// let navbar = document.getElementById("navbar");
+
+// let stickyd =navbar.offsetTop;
+// console.log(stickyd);
+// const stickyFunc = () => {
+//     if(window.pageYOffset >= stickyd){
+//         navbar.classList.add("sticky");
+//     }else{
+//         navbar.classList.remove("sticky");
+//     }
+// }
+
+// window.onscroll = () => {stickyFunc()};
+// console.log(window.onscroll = () =>{stickyFunc()});
